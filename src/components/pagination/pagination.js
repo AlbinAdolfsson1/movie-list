90b30929a87321f512b8961881bd78bc19719d80
Albin Adolfsson <albinadolfsson@MBPsomtrcodebet.localdomain>
2021-09-08 16:47:51 +0200

    Pagination cleaning

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -3,50 +3,22 @@ import cn from 'classnames'
 
 const Pagination = ( { setPageNumber, pageNumber, maxPage } ) => {
 
-    useEffect(() => {
+    const btns = []
 
-    }, [])
+    if (pageNumber > 1){
+        btns.push(<button className={cn('Page-button', {'Page-button-active': pageNumber === 1, })} onClick={() => setPageNumber(1)}>First</button>)
+    }
     
-    return (
-        <div>
-                {pageNumber >= 3 && pageNumber < maxPage-2 &&(
-                    <div>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 1, })} onClick={() => setPageNumber(1)}>First</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === pageNumber-2, })} onClick={() => setPageNumber(pageNumber-2)}>{pageNumber-2}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === pageNumber-1, })} onClick={() => setPageNumber(pageNumber-1)}>{pageNumber-1}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === pageNumber})} onClick={() => setPageNumber(pageNumber)}>{pageNumber}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === pageNumber+1, })} onClick={() => setPageNumber(pageNumber+1)}>{pageNumber+1}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === pageNumber+2, })} onClick={() => setPageNumber(pageNumber+2)}>{pageNumber+2}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage, })} onClick={() => setPageNumber(maxPage)}>Last</button>
-                    </div>
-                )}
+    for (let x = Math.max(1, pageNumber-2); x<= Math.min(pageNumber+2, maxPage); x++) {
+        btns.push(<button className={cn('Page-button', {'Page-button-active': pageNumber === x, })} onClick={() => setPageNumber(x)}>{x}</button>)
+    }
 
-                {pageNumber < 3 &&(
-                    <div>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 1, })} onClick={() => setPageNumber(1)}>First</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 1, })} onClick={() => setPageNumber(1)}>{1}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 2, })} onClick={() => setPageNumber(2)}>{2}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 3, })} onClick={() => setPageNumber(3)}>{3}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 4, })} onClick={() => setPageNumber(4)}>{4}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 5, })} onClick={() => setPageNumber(5)}>{5}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage, })} onClick={() => setPageNumber(maxPage)}>Last</button>
-                    </div>
-                )}
+    if (pageNumber < maxPage){
+        btns.push(<button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage, })} onClick={() => setPageNumber(maxPage)}>Last</button>)
+    }
 
-                {pageNumber >= maxPage-2 &&(
-                    <div>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === 1, })} onClick={() => setPageNumber(1)}>First</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage -4 })} onClick={() => setPageNumber(maxPage -4)}>{maxPage -4}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage -3 })} onClick={() => setPageNumber(maxPage -3)}>{maxPage -3}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage -2 })} onClick={() => setPageNumber(maxPage -2)}>{maxPage -2}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage -1 })} onClick={() => setPageNumber(maxPage -1)}>{maxPage -1}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage})} onClick={() => setPageNumber(maxPage)}>{maxPage}</button>
-                        <button className={cn('Page-button', {'Page-button-active': pageNumber === maxPage, })} onClick={() => setPageNumber(maxPage)}>Last</button>
-                    </div>
-                )}
-            
-        </div>
-    )
+    return <div>{btns}</div>
+
 }
 
 export default Pagination
